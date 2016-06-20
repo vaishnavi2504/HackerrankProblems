c57007d2736ae5843b90a3e1a63fa00193a69e59
vaishnavi2504 <vaishnavibhat26@gmail.com>
2016-06-20 16:49:29 -0500

    Create AlternatingCharacters.java

---
+++ AlternatingCharacters.java
@@ -0,0 +1,27 @@
+//Link to the problem-  https://www.hackerrank.com/challenges/alternating-characters
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    
+    public static int numDel(String s){
+        int count=0;
+        for(int i=0;i+1<s.length();i++){
+            if(s.charAt(i)==s.charAt(i+1))count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int v=0;v<t;v++){
+            String ip=sc.next();
+            System.out.println(numDel(ip));
+        }
+    }
+}
