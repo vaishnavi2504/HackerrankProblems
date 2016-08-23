c14443dca3435c2b979af91b79e6220a09d8cfb1
vaishnavi2504 <vaishnavibhat26@gmail.com>
2016-08-22 23:34:59 -0500

    Create DiagonalDiff.java

---
+++ DiagonalDiff.java
@@ -0,0 +1,40 @@
+// https://www.hackerrank.com/challenges/diagonal-difference
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[][] = new int[n][n];
+        for(int a_i=0; a_i < n; a_i++){
+            for(int a_j=0; a_j < n; a_j++){
+                a[a_i][a_j] = in.nextInt();
+            }
+        }
+        
+        int sum1=0;
+        for(int a_i=0; a_i < n; a_i++){
+            for(int a_j=0; a_j < n; a_j++){
+                if(a_i==a_j){
+                    sum1+=a[a_i][a_j];
+                }                
+            }
+        }
+        
+        int i=0,sum2=0;
+        int j=n-1;
+        while(i<n&&j>=0){
+            sum2+=a[i++][j--];
+        }
+        System.out.println(Math.abs(sum1-sum2));
+        
+        
+    }
+}
